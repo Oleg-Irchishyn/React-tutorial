0a0fab7aa3471191b372e9ef97b161828a01ce3a
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-02-04 10:29:40 +0200

    lesson #42 redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import store from "./redux/state"
+import store from "./redux/redux-store"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
+      <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
     </BrowserRouter>, document.getElementById('root'));
 
 }
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+  let state = store.getState();
+  rerenderEntireTree(state);
+}
+);
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,7 +1,17 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        { id: 1, message: 'Hi, How are you?', likesCount: 0 },
+        { id: 2, message: 'It is my new post', likesCount: 23 },
+        { id: 3, message: 'Blabla', likesCount: 8 },
+        { id: 4, message: 'Dada', likesCount: 11 }
+    ],
+    newPostText: "it-kamasutra.com"
+};
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
@@ -16,8 +26,8 @@ const profileReducer = (state, action) => {
         case UPDATE_NEW_POST_TEXT:
             state.newPostText = action.newText;
             return state;
-        default: 
-        return state;
+        default:
+            return state;
     }
 }
 
