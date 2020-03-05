9990bbe4f00bd270f1fb2e89dbfa563c4570bb7f
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-03-05 10:18:51 +0200

    #lesson 49 coded users component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import Users from './components/Users/Users';
+import UsersContainer from './components/Users/UsersContainer';
 import './App.css';
 import { Route } from 'react-router-dom';
 
@@ -11,15 +11,15 @@ import { Route } from 'react-router-dom';
 
 const App = (props) => {
   return (
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route path="/dialogs" render={ () => <DialogsContainer/>} />
-          <Route path="/profile" render={ () => <Profile/>}  />
-          <Route path="/users" render={ () => <Users/>}  />
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Route path="/dialogs" render={() => <DialogsContainer />} />
+        <Route path="/profile" render={() => <Profile />} />
+        <Route path="/users" render={() => <UsersContainer />} />
       </div>
+    </div>
   );
 }
 
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,11 +1,9 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        { id: 1, followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, followed: true, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Moscow', country: 'Russia' } },
-        { id: 3, followed: false, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Kyiv', country: 'Ukraine' } }
     ]
 };
 
@@ -31,6 +29,11 @@ const usersReducer = (state = initialState, action) => {
                     return u
                 })
             }
+        case SET_USERS:
+            return {
+                ...state,
+                users: [...state.users, ...action.users]
+            }
         default:
             return state;
     }
@@ -38,5 +41,6 @@ const usersReducer = (state = initialState, action) => {
 
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
+export const setUsersAC = (users) => ({ type: SET_USERS, users });
 
 export default usersReducer;
