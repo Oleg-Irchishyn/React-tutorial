2521964b91d3648d00641842ca2266116eea870c
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-03-11 11:25:17 +0200

    lesson 53 class component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import s from './Navbar.module.css';
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -11,6 +11,9 @@ const Navbar = () => {
             </div>
             <div className={s.item}>
                 <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
             </div>
             <div className={s.item}>
                 <a>News</a>
