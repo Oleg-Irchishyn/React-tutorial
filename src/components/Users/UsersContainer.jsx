90b0383af070e5deb76c0b649849f2da48fd79b1
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-03-16 11:08:45 +0200

    lesson 56

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,4 @@
-import Users from "./Users";
+import UsersAPIComponent from "./UsersAPIComponent";
 import { connect } from 'react-redux';
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/usersReducer';
 
@@ -32,6 +32,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 
 export default UsersContainer;
