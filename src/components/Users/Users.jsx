2612416d41796a176de88be9e85e08514355d011
Oleg <oleg.irchishyn91@gmail.com>
2020-03-19 12:12:49 +0200

    lesson 56 adding new container component above the presentational one in order to send ajax requests to server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,47 +3,49 @@ import styles from './users.module.css';
 import userPhoto from "../../assets/images/user.jpg"
 
 let Users = (props) => {
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    let pages = [];
+  let pages = [];
 
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-    return (
-        <div>
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
+  return (
+    <div>
+      <div className={styles.paginationWrapper}>
+        {pages.map(p => {
+          return (
+            <span className={props.currentPage === p && styles.selectedPage}
+              onClick={(e) => { props.onPageChanged(p) }}>{p}
+            </span>
+          )
+        })}
+      </div>
+      <div className={styles.usersList}>
+        {props.users.map(u => <div className={styles.usersList__item} key={u.id}>
+          <span>
             <div>
-                {pages.map(p => {
-                    return (
-                        <span className={props.currentPage === p && styles.selectedPage}
-                            onClick={(e) => { onPageChanged(p) }}>{p}
-                        </span>
-                    )
-                })}
+              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto} />
             </div>
-            {props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.usersPhoto} />
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
-                            : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-                    </div>
-                </span>
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-                <span>
-                    <div>{"u.location.country"}</div>
-                    <div>{"u.location.city"}</div>
-                </span>
-            </div>)
-            }
-        </div>
-    )
+            <div>
+              {u.followed
+                ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button>
+                : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+            </div>
+          </span>
+          <span>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
+          </span>
+          <span>
+            <div>{"u.location.country"}</div>
+            <div>{"u.location.city"}</div>
+          </span>
+        </div>)
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Users;
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -6,52 +6,52 @@ const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 
 let initialState = {
-    users: [],
-    pageSize: 5,
-    totalUsersCount: 0,
-    currentPage: 1
+  users: [],
+  pageSize: 50,
+  totalUsersCount: 0,
+  currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case FOLLOW:
-            return {
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return { ...u, followed: true }
-                    }
-                    return u
-                })
-            }
-        case UNFOLLOW:
-            return {
-                ...state,
-                users: state.users.map(u => {
-                    if (u.id === action.userId) {
-                        return { ...u, followed: false }
-                    }
-                    return u
-                })
-            }
-        case SET_USERS:
-            return {
-                ...state,
-                users: action.users
-            }
-        case SET_CURRENT_PAGE:
-            return {
-                ...state,
-                currentPage: action.currentPage
-            }
-            case SET_TOTAL_USERS_COUNT:
-                return {
-                    ...state,
-                    totalUsersCount: action.count
-                }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case FOLLOW:
+      return {
+        ...state,
+        users: state.users.map(u => {
+          if (u.id === action.userId) {
+            return { ...u, followed: true }
+          }
+          return u
+        })
+      }
+    case UNFOLLOW:
+      return {
+        ...state,
+        users: state.users.map(u => {
+          if (u.id === action.userId) {
+            return { ...u, followed: false }
+          }
+          return u
+        })
+      }
+    case SET_USERS:
+      return {
+        ...state,
+        users: action.users
+      }
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.currentPage
+      }
+    case SET_TOTAL_USERS_COUNT:
+      return {
+        ...state,
+        totalUsersCount: action.count
+      }
+    default:
+      return state;
+  }
 }
 
 export const followAC = (userId) => ({ type: FOLLOW, userId });
