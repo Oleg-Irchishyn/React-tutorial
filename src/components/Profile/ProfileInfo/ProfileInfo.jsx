4e08def70c927947048ea3cc6e8056564ab36667
Oleg <oleg.irchishyn91@gmail.com>
2020-03-22 17:53:37 +0200

    lesson 59 profile page api

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import s from './ProfileInfo.module.css';
+import PreloaderImg from '../../common/Preloader/PreloaderImg';
 
 
-const ProfileInfo = () => {
-    return (
-        <div>
-            <div>
-                <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-            </div>
-            <div className={s.descriptionBlock}>
+const ProfileInfo = (props) => {
+  if (!props.profile) {
+    return <PreloaderImg />
+  }
+  return (
+    <div>
+      <div>
+        <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+      </div>
+      <div className={s.descriptionBlock}>
+        <img alt="infoPhoto" src={props.profile.photos.large} />
                 ava + description
             </div>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default ProfileInfo;
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
   posts: [
@@ -8,7 +9,8 @@ let initialState = {
     { id: 3, message: 'Blabla', likesCount: 8 },
     { id: 4, message: 'Dada', likesCount: 11 }
   ],
-  newPostText: "it-kamasutra.com"
+  newPostText: "it-kamasutra.com",
+  profile: null
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -29,12 +31,18 @@ const profileReducer = (state = initialState, action) => {
         ...state,
         newPostText: action.newText
       }
+    case SET_USER_PROFILE:
+      return {
+        ...state,
+        profile: action.profile
+      }
     default:
       return state;
   }
 }
 
 export const AddPostActionCreator = () => ({ type: ADD_POST });
+export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
 export const updatedNewPostTextActionCreator = (text) =>
   ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
