dafc00084e39300cb91c3fad78bc1ed55dc87824
Oleg <oleg.irchishyn91@gmail.com>
2020-04-08 12:51:18 +0300

    lesson 71 localState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import PreloaderImg from '../../common/Preloader/PreloaderImg';
-
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -9,13 +9,13 @@ const ProfileInfo = (props) => {
   }
   return (
     <div>
-      <div>
+      {/*<div>
         <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-      </div>
+      </div>*/}
       <div className={s.descriptionBlock}>
         <img alt="infoPhoto" src={props.profile.photos.large} />
-                ava + description
-            </div>
+        <ProfileStatus status={"Hello My friends"} />
+      </div>
     </div>
   )
 }
