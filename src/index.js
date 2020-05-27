38235e86fce587d2332d0090e4013b22bc2fb649
Oleg <oleg.irchishyn91@gmail.com>
2020-05-27 12:10:42 +0300

    Lesson 95 added HashRouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter /*basename={process.env.PUBLIC_URL} - это строка нужна для BrowserRouter для github pages*/>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
 
