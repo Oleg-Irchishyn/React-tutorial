9d5bb59d92ce508858caf6c883cfa55fb6fe2c54
Oleg Irchishyn <o.irchishyn@smileexpo.com.ua>
2020-09-04 10:01:11 +0300

    added i18next module dependency

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import store from "./redux/redux-store"
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
+import './i18next';
+import PreloaderImg from "./components/common/Preloader/PreloaderImg";
 
 
 ReactDOM.render(
   <HashRouter /*basename={process.env.PUBLIC_URL} - это строка нужна для BrowserRouter для github pages*/>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={<PreloaderImg />}>
+        <App />
+      </Suspense>
     </Provider>
   </HashRouter>, document.getElementById('root'));
 
